27c7d58dd47a01c9b59a8a1e603fdaadfbb551d2
juliana-happatsch <jhappatsch@gmail.com>
2021-12-06 17:21:43 -0300

    Add engine

---
+++ js/functions.js
@@ -0,0 +1,15 @@
+const engine = {
+  colors: ['green', 'purple', 'pink', 'yellow', 'red', 'blue', 'white', 'black', 'grey'],
+  hex: {
+    green: '#008000',
+    purple: '#800080',
+    pink: '#FFC0CB',
+    yellow: '#FFFF00',
+    red: '#FF0000',
+    blue: '#0000FF',
+    white: '#FFFFFF',
+    black: '#000000',
+    grey: '#808080',
+  },
+  coins: 0,
+};
